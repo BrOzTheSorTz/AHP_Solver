8f3c7004e848f6cef54c93a2f23410dfa781b233
Jose Pablo Soriano Torres <jpst0002@red.ujaen.es>
2024-11-14 17:43:39 +0100

    [ADD] Server Flask and application web

--- main.py
+++ main.py
@@ -1,8 +1,63 @@
+from flask import Flask, request, render_template, redirect, url_for
+import numpy as np
+import pandas as pd
 from models import AHP
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    ahp_method = AHP()
-    ahp_method.obtain_ranking()
+app = Flask(__name__)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Función para procesar los archivos y calcular AHP
+def calcular_ahp(alternatives_path, criterios_path):
+    ahp_method = AHP(criterios_path, alternatives_path)
+    ranking, info_consistencia = ahp_method.obtain_ranking()
+
+    return ranking, info_consistencia
+
+
+# Ruta principal para cargar archivos y procesar datos
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Obtener archivos cargados
+        alt_file = request.files.get('alternatives')
+        crit_file = request.files.get('criterios')
+
+        # Guardar temporalmente los archivos
+        alt_path = './tmp/alternatives.csv'
+        crit_path = './tmp/criterios.csv'
+        alt_file.save(alt_path)
+        crit_file.save(crit_path)
+
+        # Calcular AHP
+        ranking, info_consistencia = calcular_ahp(alt_path, crit_path)
+        name_ranking = []
+        i = 1
+        for n in ranking:
+            name_alternative = "A" + str(i)
+            i = i + 1
+            name_ranking.append([name_alternative, n])
+        name_ranking_order = sort_alternatives(name_ranking)
+        # Redirigir a la página de resultados con los datos calculados
+        return render_template('result.html', ranking=name_ranking_order, info=info_consistencia)
+
+    return render_template('index.html')
+
+
+def sort_alternatives(name_ranking):
+    """
+    :param name_ranking: List of lists, e.g., [[A1,0.34],[A2,0.02],...]
+    :return: name_ranking sorted in descending order by ranking values
+    """
+
+    n = len(name_ranking)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if name_ranking[j][1] < name_ranking[j + 1][1]:  # Orden descendente
+                # Intercambia elementos
+                name_ranking[j], name_ranking[j + 1] = name_ranking[j + 1], name_ranking[j]
+
+    return name_ranking
+
+# Ejecutar la aplicación
+if __name__ == '__main__':
+    app.run(debug=True)
--- models/AHP.py
+++ models/AHP.py
@@ -58,37 +58,37 @@ class AHP:
         self.num_criterios = None
         self.criterios = None
         self.alternativas = None
-        self.random_index = {3:0.58,4:0.9,5:1.12,6:1.24,7:1.32,8:1.41,9:1.45,10:1.49}
+        self.random_index = {3: 0.58, 4: 0.9, 5: 1.12, 6: 1.24, 7: 1.32, 8: 1.41, 9: 1.45, 10: 1.49}
+
 
         self.read_criterios(criterios_path)
-        self.read_alternativas(alternatives_path)
 
+        self.read_alternativas(alternatives_path)
 
     def obtain_ranking(self):
         info_matrix = {}
         e = Eigenvalue()
         # Obtain the weights for the criterios matrix
         eigenvalue, weights_criterios = e.obtain_eigenvector_normalized(self.criterios)
-        ci = (eigenvalue-self.num_criterios)/(self.num_criterios-1)
+        ci = (eigenvalue - self.num_criterios) / (self.num_criterios - 1)
         ri = self.random_index[self.num_criterios]
-        info_matrix['C']=[eigenvalue,ci,ri,ci/ri]
+        info_matrix['C'] = [eigenvalue, ci, ri, ci / ri]
         # Obtain the weights for the alternatives
         weights_alternatives = []
         id = "AC"
-        i=1
+        i = 1
         for alternatives in self.alternativas:
             eigenvalue, weight = e.obtain_eigenvector_normalized(alternatives)
 
             ci = (eigenvalue - self.num_alternativas) / (self.num_alternativas - 1)
             ri = self.random_index[self.num_alternativas]
-            info_matrix[id + str(i)] = [eigenvalue,ci,ri,ci/ri]
+            info_matrix[id + str(i)] = [eigenvalue, ci, ri, ci / ri]
             weights_alternatives.append(weight)
-            i = i+1
+            i = i + 1
         weights_alternatives = np.array(weights_alternatives)
 
-        ranking = np.dot(np.transpose(weights_alternatives),weights_criterios)
+        ranking = np.dot(np.transpose(weights_alternatives), weights_criterios)
+        ranking = [round(float(n),3) for n in ranking]
 
         print(f"Ranking: {ranking}. Info Consitency: {info_matrix}")
-        return ranking
-
-
+        return ranking, info_matrix
